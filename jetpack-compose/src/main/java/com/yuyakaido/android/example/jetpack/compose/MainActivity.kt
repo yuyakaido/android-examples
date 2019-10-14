3aa1250bdeb767f1a4ca72722c92858479df5f6a
Yuya Kaido <yuyakaido@gmail.com>
2019-10-14 20:14:50 +0900

    Add example to create own component of jetpack compose

--- jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
+++ jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yuyakaido.android.example.jetpack.compose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.core.sp
@@ -11,19 +12,24 @@ import androidx.ui.text.font.FontWeight
 import androidx.ui.text.style.TextDecoration
 
 class MainActivity : AppCompatActivity() {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      Text(
-        text = "Hello Jetpack Compose",
-        style = TextStyle(
-          color = Color.DarkGray,
-          fontSize = 28.sp,
-          fontWeight = FontWeight.bold,
-          background = Color.LightGray,
-          decoration = TextDecoration.Underline
-        )
-      )
-    }
+    setContent { styledText("Hello Jetpack Compose") }
   }
+
+  @Composable
+  private fun styledText(text: String) {
+    return Text(
+      text = text,
+      style = TextStyle(
+        color = Color.DarkGray,
+        fontSize = 28.sp,
+        fontWeight = FontWeight.bold,
+        background = Color.LightGray,
+        decoration = TextDecoration.Underline
+      )
+    )
+  }
+
 }
