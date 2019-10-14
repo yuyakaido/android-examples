e9a023f43f91cb216f959c1e92791ea8cdcc6869
Yuya Kaido <yuyakaido@gmail.com>
2019-10-14 19:58:40 +0900

    Add text style example of jetpack compose

--- jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
+++ jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.core.sp
+import androidx.ui.graphics.Color
+import androidx.ui.text.TextStyle
+import androidx.ui.text.font.FontWeight
+import androidx.ui.text.style.TextDecoration
 
 class MainActivity : AppCompatActivity() {
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { Text("Hello Jetpack Compose") }
+    setContent {
+      Text(
+        text = "Hello Jetpack Compose",
+        style = TextStyle(
+          color = Color.DarkGray,
+          fontSize = 28.sp,
+          fontWeight = FontWeight.bold,
+          background = Color.LightGray,
+          decoration = TextDecoration.Underline
+        )
+      )
+    }
   }
-
 }
