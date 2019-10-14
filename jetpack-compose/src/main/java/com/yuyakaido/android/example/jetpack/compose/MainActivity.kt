201716343e573c1af839ba1f34b5c21e589b0e64
Yuya Kaido <yuyakaido@gmail.com>
2019-10-14 21:17:40 +0900

    [Jetpack Compose] Add example for state mutation

--- jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
+++ jetpack-compose/src/main/java/com/yuyakaido/android/example/jetpack/compose/MainActivity.kt
@@ -3,12 +3,16 @@ package com.yuyakaido.android.example.jetpack.compose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.state
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
-import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.core.sp
 import androidx.ui.graphics.Color
-import androidx.ui.layout.Padding
+import androidx.ui.layout.Column
+import androidx.ui.material.Button
+import androidx.ui.material.MaterialColors
+import androidx.ui.material.MaterialTheme
 import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontWeight
 
@@ -17,8 +21,17 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Padding(16.dp) {
-        styledText("Hello Jetpack Compose")
+      val count = +state { 0 }
+      MaterialTheme {
+        Column {
+          Button(
+            text = "Increment",
+            onClick = {
+              count.value++
+            }
+          )
+          styledText("Count: ${count.value}")
+        }
       }
     }
   }
